ee418aac955994fc1ac36855f94863209b00c98a
RichardLara <richardgrlara@gmail.com>
2024-08-12 21:22:52 -0300

    Update index.html and style.css, create cadastro.js

---
+++ cadastro.js
@@ -0,0 +1,66 @@
+//VERIFICVAÇÃO DE CADASTRO
+const form = document.getElementById('form')
+const nome = document.getElementById('nome')
+const nasc = document.getElementById('nascimento')
+const email = document.getElementById('email')
+const fone = document.getElementById('fone')
+const user = document.getElementById('username')
+const pass1 = document.getElementById('senha1')
+const pass2 = document.getElementById('senha2')
+let cadastro = 0
+
+
+
+function checkingInputs () {
+    //VALIDAÇÃO DO NOME COMPLETO
+    nome.addEventListener("blur", (e) => {
+        console.log(nome.value);
+    
+    })
+
+    //VALIDAÇÂO DA DATA DE NASCIMENTO
+    nasc.addEventListener("blur", (e) => {
+        console.log(nasc.value);
+    
+    })
+
+    //VALIDAÇÃO DO EMAIL
+    email.addEventListener("blur", (e) => {
+        console.log(email.value);
+    
+    })
+
+    //VALIDAÇÃO DE FONE
+    fone.addEventListener("blur", (e) => {
+        console.log(fone.value);
+    
+    })
+
+    //VALIDAÇÃO DE USERNAME
+    user.addEventListener("blur", (e) => {
+        console.log(user.value);
+    
+    })
+
+    //VALIDAÇÃO DA 1° SENHA
+    pass1.addEventListener("blur", (e) => {
+        console.log(pass1.value);
+    
+    })
+
+    //VALIDAÇÃO DA 2° SENMHA
+    pass2.addEventListener("blur", (e) => {
+        console.log(pass2.value);
+    
+    })
+
+    //CADASTRAR
+  
+}
+
+checkingInputs()
+form.addEventListener('submit', (e) => {
+    //e.preventDefault()
+    //checkInputs()
+    //confere()
+    })
